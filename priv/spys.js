db64e2364dacb495170ce4bc36b7bd1471e96738
tim <llStrainll@gmail.com>
2016-02-26 15:06:56 +0300

    wait while proxy page is loaded

--- priv/spys.js
+++ priv/spys.js
@@ -1,18 +1,25 @@
 var page = require('webpage').create();
 var system = require('system');
-page.open('http://spys.ru/proxys/'+system.args[1]+'/', function(status) {
-	page.onLoadFinished = function(_){
-		var arr = page.evaluate(function(){return [].slice.call(document.getElementsByClassName('spy14'))
-			.map(function(el){return el.textContent.match(/^(\d+\.\d+\.\d+\.\d+).+(\:\d+)$/);})
-			.filter(function(el){return el;})
-			.map(function(el){return (el[1]+el[2]);})
-				;});
-		console.log(JSON.stringify(arr));
-		phantom.exit();
+var submitted = false;
+page.onInitialized = function() {
+	page.onCallback = function(data) {
+		if(submitted){
+			var arr = page.evaluate(function(){return [].slice.call(document.getElementsByClassName('spy14'))
+				.map(function(el){return el.textContent.match(/^(\d+\.\d+\.\d+\.\d+).+(\:\d+)$/);})
+				.filter(function(el){return el;})
+				.map(function(el){return (el[1]+el[2]);})
+					;});
+			console.log(JSON.stringify(arr));
+			phantom.exit();
+		}else{
+			page.evaluate(function(){
+				document.getElementById('xpp').selectedIndex = 3;
+				document.getElementById('xf2').selectedIndex = 1;
+				document.forms[0].submit();
+			});
+			submitted = true;
+		};
 	};
-	page.evaluate(function(){
-		document.getElementById('xpp').selectedIndex = 3;
-		document.getElementById('xf2').selectedIndex = 1;
-		document.forms[0].submit();
-	});
-});
+	page.evaluate(function(){document.addEventListener('DOMContentLoaded', function(){window.callPhantom();}, false);});
+};
+page.open('http://spys.ru/proxys/'+system.args[1]+'/', function(_){});
